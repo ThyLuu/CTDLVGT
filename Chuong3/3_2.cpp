e78e8405e3113c6efa852306a929c67aa1295a2a
ThyLuu <luuthythy2003@gmail.com>
2023-03-07 15:45:09 +0700

    7/3

---
+++ Chuong3/3_2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+#define Max 100
+int ketthuc(int n){
+	if(n==0){
+		return 0;
+	}
+}
+int main(){
+	int n;
+    int a[Max];
+    cout << "Nhap so luong phan tu: ";
+    cin >> n;
+    if(n<=0){
+    	return ketthuc(n);
+	}
+    for(int i = 0; i < n; i++){
+        cout <<"Nhap a["<<i<<"] = ";
+        cin >> a[i];
+    }
+    
+    int sx;
+    for(int i = 0; i < n - 1; i++){
+        for(int j = i + 1; j < n; j++){
+            if(a[i] < a[j]){ // giam dan thi < 
+                // Hoan vi giua a[i] va a[j]
+                sx = a[i];
+                a[i] = a[j];
+                a[j] = sx;        
+            }
+        }
+    }
+    
+    cout<<"Mang sau khi da sap xep lai: ";
+    for(int i = 0; i < n; i++){
+        cout<<" " << a[i];
+    }   
+}
